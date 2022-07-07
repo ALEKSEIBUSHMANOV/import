80faed59a052dfc21e0e9664392e32f38017fe69
ALEKSEIBUSHMANOV <lesha.bushmanov@yandex.ru>
2022-07-07 22:58:51 +0300

    Добавление файлов

---
+++ main.py
@@ -0,0 +1,8 @@
+from application.people import get_employees
+from application.salary import calculate_salary
+from datetime import date
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    print(date.today())
